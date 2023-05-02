83c8764bfb1349bceb715264c397ffde26af7b39
yilmazbey001 <yilmazhaciosmanoglu123@gmail.com>
2023-05-02 16:20:44 +0300

    C# Mayın Tarlası
    C# Görsel Programlama Dersimden sonra kendimi denemek için yapmış olduğum Mayın Tarlası Oyunu.

---
+++ MayınTarlasi1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace MayınTarlasi1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.BaslaButton = new System.Windows.Forms.Button();
+            this.YeniOyunButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 190);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 780);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Kolay",
+            "Orta",
+            "Zor"});
+            this.checkedListBox1.Location = new System.Drawing.Point(267, 48);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(166, 104);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.Visible = false;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // BaslaButton
+            // 
+            this.BaslaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BaslaButton.Location = new System.Drawing.Point(12, 95);
+            this.BaslaButton.Name = "BaslaButton";
+            this.BaslaButton.Size = new System.Drawing.Size(239, 67);
+            this.BaslaButton.TabIndex = 2;
+            this.BaslaButton.Text = "Başla";
+            this.BaslaButton.UseVisualStyleBackColor = true;
+            this.BaslaButton.Visible = false;
+            this.BaslaButton.Click += new System.EventHandler(this.BaslaButton_Click);
+            // 
+            // YeniOyunButton
+            // 
+            this.YeniOyunButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.YeniOyunButton.Location = new System.Drawing.Point(12, 12);
+            this.YeniOyunButton.Name = "YeniOyunButton";
+            this.YeniOyunButton.Size = new System.Drawing.Size(239, 67);
+            this.YeniOyunButton.TabIndex = 3;
+            this.YeniOyunButton.Text = "Yeni Oyun";
+            this.YeniOyunButton.UseVisualStyleBackColor = true;
+            this.YeniOyunButton.Click += new System.EventHandler(this.YeniOyunButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(585, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(71, 38);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = ":";
+            this.textBox1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MayınTarlasi1.Properties.Resources.saat;
+            this.pictureBox2.Location = new System.Drawing.Point(512, 119);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(67, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MayınTarlasi1.Properties.Resources.bayrak;
+            this.pictureBox1.Location = new System.Drawing.Point(512, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(585, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(71, 38);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = ":";
+            this.textBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 1012);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.YeniOyunButton);
+            this.Controls.Add(this.BaslaButton);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button BaslaButton;
+        private System.Windows.Forms.Button YeniOyunButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MayınTarlasi1/MayınTarlasi1/Form1.cs
@@ -0,0 +1,521 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayınTarlasi1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MayınTarlasi MayinTarlamiz;
+        List<Mayin> mayinlarimiz;
+        Image MayinResmi = Image.FromFile(@"mayin.png");
+        Image BayrakResmi = Image.FromFile(@"bayrak.png");
+        int bulunan_temiz_alan;
+        int BayrakSayisi = 0;
+        int SayaçVePuan = 0;
+        bool sayacDur = false;
+        int toplam_alan;
+        
+
+
+
+        private void YeniOyunButton_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Visible = true;
+            panel1.Controls.Clear();
+            
+            YeniOyunBaslat();
+            textBox1.Visible = false;
+            pictureBox1.Visible = false;
+            textBox2.Visible = false;
+            pictureBox2.Visible = false;
+            panel1.Visible = false;
+            panel1.Enabled = true;
+
+        }
+
+
+
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (checkedListBox1.SelectedIndex == 0 || checkedListBox1.SelectedIndex == 1 || checkedListBox1.SelectedIndex == 2)
+            {
+                BaslaButton.Visible = true;
+                checkedListBox1.Visible = false;
+            }
+        }
+
+
+
+
+        private void BaslaButton_Click(object sender, EventArgs e)
+        {
+            BaslaButton.Visible = false;
+            YeniOyunButton.Visible = false;
+            YeniOyunBaslat();
+            textBox1.Text = (": " + BayrakSayisi);
+            textBox1.Visible = true;
+            pictureBox1.Visible = true;
+            textBox2.Visible = true;
+            pictureBox2.Visible = true;
+            panel1.Visible = true;
+            SayaçVePuan = 0;
+            sayacDur = false;
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// ///////////////////////////////////////////
+        /// </summary>
+        public class Mayin
+        {
+            Point loc;
+            bool dolu;
+            bool bakildi;
+
+            public Mayin(Point loca)
+            {
+                dolu = false;
+                loc = loca;
+            }
+
+            public Point KonumAl
+            {
+                get { return loc; }
+            }
+
+            public bool mayinVarMi
+            {
+                get
+                {
+                    return dolu;
+                }
+                set
+                {
+                    dolu = value;
+                }
+            }
+
+            public bool bakildi_
+            {
+                get
+                {
+                    return bakildi;
+                }
+                set
+                {
+                    bakildi = value;
+                }
+            }
+        }
+
+
+        //////////////////////////////////////////////////////
+
+
+        class MayınTarlasi
+        {
+            Size buyukluk_;
+            List<Mayin> mayinlar;
+            int DoluMayinSayisi;
+            Random rnd = new Random();
+
+
+
+
+
+            public MayınTarlasi(Size buyukluk, int mayin_Sayisi)
+            {
+                mayinlar = new List<Mayin>();
+                DoluMayinSayisi = mayin_Sayisi;
+                buyukluk_ = buyukluk;
+                for (int x = 0; x < buyukluk.Width; x = x + 60)
+                {
+                    for (int y = 0; y < buyukluk.Height; y = y + 60)
+                    {
+                        Mayin m = new Mayin(new Point(x, y));
+                        MayinEkle(m);
+                    }
+                }
+
+                MayinlariDoldur();
+            }
+            public void MayinEkle(Mayin m)
+            {
+                mayinlar.Add(m);
+            }
+
+
+            private void MayinlariDoldur()
+            {
+                int sayi = 0;
+                while (sayi < DoluMayinSayisi)
+                {
+                    int i = rnd.Next(0, mayinlar.Count);
+                    Mayin item = mayinlar[i];
+                    if (item.mayinVarMi == false)
+                    {
+                        item.mayinVarMi = true;
+                        sayi++;
+                    }
+                }
+            }
+
+            public Size buyuklugu
+            {
+                get
+                {
+                    return buyukluk_;
+                }
+            }
+            public Mayin MayinAlLoc(Point loc)
+            {
+                foreach (Mayin item in mayinlar)
+                {
+                    if (item.KonumAl == loc)
+                    {
+                        return item;
+                    }
+                }
+                return null;
+            }
+            public List<Mayin> GetAllMayin
+            {
+                get
+                {
+                    return mayinlar;
+                }
+            }
+            public int ToplamMayinSayisi
+            {
+                get
+                {
+                    return DoluMayinSayisi;
+                }
+            }
+           
+
+
+        }
+
+        ///////////////////////////
+
+        public void MayinEkle()
+        {
+            for (int x = 0; x < panel1.Width; x = x + 60)
+            {
+                for (int y = 0; y < panel1.Height; y = y + 60)
+                {
+                    ButtonEkle(new Point(x, y));
+                }
+            }
+        }
+
+        public void ButtonEkle(Point loc)
+        {
+            Button btn = new Button();
+            btn.Name = loc.X + "" + loc.Y;
+            btn.Size = new Size(60,60);
+            btn.Location = loc;
+            btn.Click += new EventHandler(btn_Click);
+            btn.MouseUp += new MouseEventHandler(btn_MouseUp);
+            panel1.Controls.Add(btn);
+        }
+
+        void btn_MouseUp(object sender, MouseEventArgs e)
+        {
+            Button btn = (sender as Button);
+            if (e.Button == MouseButtons.Right)
+            {
+                if (btn.BackgroundImage != BayrakResmi && BayrakSayisi>0)
+                {
+                   
+                    btn.BackgroundImage = BayrakResmi;/////////////////sağ tıkla bayrak ekleme
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
+                    BayrakSayisi--;
+                    textBox1.Text = (": " + BayrakSayisi);
+                    
+                }
+                else
+                {
+                    btn.BackgroundImage = null;
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
+                        BayrakSayisi++;
+                    textBox1.Text = (": " + BayrakSayisi);
+
+                }
+               
+
+            }
+           
+        }
+
+        public void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (sender as Button);// hangi butona tıklandığında o nun adresini bize ver
+            Mayin myn = MayinTarlamiz.MayinAlLoc(btn.Location);
+            mayinlarimiz = new List<Mayin>();
+            if (myn.mayinVarMi)
+            {
+                sayacDur = true;
+                MessageBox.Show("Kaybettinz.");
+                YeniOyunButton.Visible = true;
+                Mayinlari_goster();
+                panel1.Enabled = false;
+            }
+
+            else
+            { 
+                int s = EtraftaKacMayinVar(myn);
+                if (s == 0)
+                {
+
+                    mayinlarimiz.Add(myn);
+                    for (int i = 0; i < mayinlarimiz.Count; i++)
+                    {
+                        Mayin item = mayinlarimiz[i];
+                        if (item != null)
+                        {
+                            if (item.bakildi_ == false && item.mayinVarMi == false)
+                            {
+                                Button btnx = (Button)panel1.Controls.Find(item.KonumAl.X + "" + item.KonumAl.Y, false)[0];
+                                if (EtraftaKacMayinVar(mayinlarimiz[i]) == 0)
+                                {
+
+                                    btnx.Enabled = false;
+
+                                    cevresindekileri_ekle(item);
+                                }
+                                else
+                                {
+                                    btnx.Text = EtraftaKacMayinVar(item).ToString();
+
+                                }
+                                bulunan_temiz_alan++;
+                                item.bakildi_ = true;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    btn.Text = s.ToString();
+                    bulunan_temiz_alan++;
+                }
+
+            }
+            if (bulunan_temiz_alan >= toplam_alan - MayinTarlamiz.ToplamMayinSayisi)
+            {
+                sayacDur = true;
+                MessageBox.Show("kazandiniz!!\n"+"Skorunuz:"+ SayaçVePuan);
+                Mayinlari_goster();
+            }
+
+        }
+
+
+
+        public int EtraftaKacMayinVar(Mayin m)
+        {
+            int sayi = 0;
+            if (m.KonumAl.X > 0)
+            {
+                if (MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X - 60, m.KonumAl.Y)).mayinVarMi)
+                {
+                    sayi++;
+                }
+            }
+            if (m.KonumAl.Y <( panel1.Height -60) && m.KonumAl.X < (panel1.Width-60 ))   
+            {
+                if (MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X + 60, m.KonumAl.Y + 60)).mayinVarMi)
+                {
+                    sayi++;
+
+                }
+            }
+            
+            if (m.KonumAl.X < panel1.Width - 60)
+            {
+                if (MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X + 60, m.KonumAl.Y)).mayinVarMi)
+                {
+                    sayi++;
+                }
+            }
+            if (m.KonumAl.X > 0 && m.KonumAl.Y > 0)
+            {
+                if (MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X - 60, m.KonumAl.Y - 60)).mayinVarMi)
+                {
+                    sayi++;
+                }
+            }
+            if (m.KonumAl.Y > 0)
+            {
+                if (MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X, m.KonumAl.Y - 60)).mayinVarMi)
+                {
+                    sayi++;
+                }
+            }
+            if (m.KonumAl.X > 0 && m.KonumAl.Y < panel1.Height - 60)
+            {
+                if (MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X - 60, m.KonumAl.Y + 60)).mayinVarMi)
+                {
+                    sayi++;
+                }
+            }
+            if (m.KonumAl.Y < panel1.Height - 60)
+            {
+                if (MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X, m.KonumAl.Y + 60)).mayinVarMi)
+                {
+                    sayi++;
+                }
+            }
+            if (m.KonumAl.X > panel1.Width - 60 && m.KonumAl.Y > 0)
+            {
+                if (MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X + 60, m.KonumAl.Y - 60)).mayinVarMi)
+                {
+                    sayi++;
+                }
+            }
+
+            return sayi;
+        }
+
+
+        public void cevresindekileri_ekle(Mayin m)
+        {
+            bool b1 = false;
+            bool b2 = false;
+            bool b3 = false;
+            bool b4 = false;
+            if (m.KonumAl.X > 0)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X - 60, m.KonumAl.Y)));
+                b1 = true;
+            }
+            if (m.KonumAl.Y > 0)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X, m.KonumAl.Y - 60)));
+                b2 = true;
+            }
+            if (m.KonumAl.X < panel1.Width)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X + 60, m.KonumAl.Y)));
+                b3 = true;
+            }
+            if (m.KonumAl.Y < panel1.Height)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X, m.KonumAl.Y + 60)));
+                b4 = true;
+            }
+
+
+
+            if (b1 && b2)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X - 60, m.KonumAl.Y - 60)));
+            }
+            if (b1 && b4)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X - 60, m.KonumAl.Y + 60)));
+            }
+            if (b2 && b3)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X + 60, m.KonumAl.Y - 60)));
+            }
+            /////
+            /* if(b3&& b4)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X + 60, m.KonumAl.Y + 60)));
+            }
+            */
+            if (b2 && b4)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X , m.KonumAl.Y + 60)));
+            }
+            if(b3&& b4)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X + 60, m.KonumAl.Y + 60)));
+            }
+            if (b1 && b3)
+            {
+                mayinlarimiz.Add(MayinTarlamiz.MayinAlLoc(new Point(m.KonumAl.X - 60, m.KonumAl.Y )));
+            }
+        }
+        public void Mayinlari_goster()
+        {
+            foreach (Mayin item in MayinTarlamiz.GetAllMayin)
+            {
+                if (item.mayinVarMi)
+                {
+                    Button btn = (Button)panel1.Controls.Find(item.KonumAl.X + "" + item.KonumAl.Y, false)[0];
+                    btn.BackgroundImage = MayinResmi;
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
+                }
+            }
+        }
+
+
+        private void YeniOyunBaslat()
+            {
+                
+
+                if (checkedListBox1.SelectedIndex == 0)
+                {
+                    MayinTarlamiz = new MayınTarlasi(new Size(600, 480), 10);
+                    panel1.Size = MayinTarlamiz.buyuklugu;
+                    bulunan_temiz_alan = 0;
+                    BayrakSayisi = 10;
+                    toplam_alan = 80;
+                    MayinEkle();
+                }
+                if (checkedListBox1.SelectedIndex == 1)
+                {
+                    MayinTarlamiz = new MayınTarlasi(new Size(720,720),20 );
+                    panel1.Size = MayinTarlamiz.buyuklugu;
+                    bulunan_temiz_alan = 0;
+                    BayrakSayisi = 20;
+                    toplam_alan = 144;
+                    MayinEkle();
+
+                }
+
+                if (checkedListBox1.SelectedIndex == 2)
+                {
+                    MayinTarlamiz = new MayınTarlasi(new Size(900, 720), 25);
+                    panel1.Size = MayinTarlamiz.buyuklugu;
+                    bulunan_temiz_alan = 0;
+                    BayrakSayisi = 25;
+                    toplam_alan = 180;
+                    MayinEkle();
+                }
+
+
+            }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (sayacDur == false)
+            {
+                SayaçVePuan++;
+                textBox2.Text = (": " + SayaçVePuan);
+            }
+        }
+    }
+}
